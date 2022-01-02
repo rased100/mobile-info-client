22d737bdaf37b033494e290a9304a9a94d26bac5
rased100 <rased100@gmail.com>
2022-01-03 00:38:08 +0600

    first footer commit

---
+++ src/Pages/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className='bg-dark'>
+            <div className='text-white p-5'>
+                <h5>this is footer</h5>
+            </div>
+        </div>
+    );
+};
+
+export default Footer;
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import AddProduct from '../Admin/AddProduct/AddProduct';
 import Banner from '../Banner/Banner';
+import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
 import Products from '../Products/Products';
 
@@ -13,7 +14,7 @@ const Home = () => {
             <Banner></Banner>
             <Products></Products>
             <AddProduct></AddProduct>
-
+            <Footer></Footer>
         </div>
     );
 };
